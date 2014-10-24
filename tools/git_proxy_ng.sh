34314c4cac4326145deb1dddc315c9f05f1d301d
kelleyrw <kelleyrw@gmail.com>
2014-10-24 11:56:15 -0700

    added graph_walk_python

--- tools/git_proxy_ng.sh
+++ tools/git_proxy_ng.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+git config --global user.name "kelleyrw"
+git config --global user.email kelleyrw@gmail.com
 
 git config --global http.proxy http://J28976@centralproxy.northgrum.com:80
 git config --global http.sslverify false
