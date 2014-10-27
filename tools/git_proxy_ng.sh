3a1d67af5ca44c080adda1b9a1aff7e94d51be29
Ryan Kelley <kelleyrw@users.noreply.github.com>
2014-10-27 21:34:12 +0000

    Delete git_proxy_ng.sh

--- tools/git_proxy_ng.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-git config --global user.name "kelleyrw"
-git config --global user.email kelleyrw@gmail.com
-
-git config --global http.proxy http://J28976@centralproxy.northgrum.com:80
-git config --global http.sslverify false
