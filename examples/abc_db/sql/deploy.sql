87d36c0254d24a9772c142bbe6131269448fac0a
kelleyrw <kelleyrw@gmail.com>
2015-02-27 05:08:12 -0800

    updates to sync with work

--- examples/abc_db/sql/deploy.sql
+++ examples/abc_db/sql/deploy.sql
@@ -1,10 +1,11 @@
 -- drop existing
 
-drop table player_images;
 drop table players;
 drop table teams;
+drop table array_data;
 drop sequence players_id_seq;
 drop sequence teams_id_seq;
+drop sequence array_data_id_seq;
 
 -- deploy
 
