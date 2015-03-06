9da58ee7f86706e9762567767f4cbcabfe48350e
kelleyrw <kelleyrw@gmail.com>
2015-03-05 20:53:19 -0800

    ex1 in progress

--- courses/coursera_ml008/week2/mlclass-ex1/ex1.m
+++ courses/coursera_ml008/week2/mlclass-ex1/ex1.m
@@ -53,7 +53,7 @@ pause;
 fprintf('Running Gradient Descent ...\n')
 
 X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
-theta = zeros(2, 1); % initialize fitting parameters
+theta = zeros(1, 2); % initialize fitting parameters
 
 % Some gradient descent settings
 iterations = 1500;
@@ -63,7 +63,8 @@ alpha = 0.01;
 computeCost(X, y, theta)
 
 % run gradient descent
-theta = gradientDescent(X, y, theta, alpha, iterations);
+[theta, J] = gradientDescent(X, y, theta, alpha, iterations);
+n = 1:length(J)';
 
 % print theta to screen
 fprintf('Theta found by gradient descent: ');
@@ -71,15 +72,20 @@ fprintf('%f %f \n', theta(1), theta(2));
 
 % Plot the linear fit
 hold on; % keep previous plot visible
-plot(X(:,2), X*theta, '-')
+plot(X(:,2), X*theta', '-')
 legend('Training data', 'Linear regression')
 hold off % don't overlay any more plots on this figure
 
+figure
+plot(n, J)
+xlabel('trial')
+ylabel('J(\theta)^{trial}')
+
 % Predict values for population sizes of 35,000 and 70,000
-predict1 = [1, 3.5] *theta;
+predict1 = [1, 3.5] *theta';
 fprintf('For population = 35,000, we predict a profit of %f\n',...
     predict1*10000);
-predict2 = [1, 7] * theta;
+predict2 = [1, 7] * theta';
 fprintf('For population = 70,000, we predict a profit of %f\n',...
     predict2*10000);
 
--- courses/coursera_ml008/week2/mlclass-ex1/plotData.m
+++ courses/coursera_ml008/week2/mlclass-ex1/plotData.m
@@ -15,6 +15,9 @@ function plotData(x, y)
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
 figure; % open a new figure window
+plot(x, y, 'rx', 'MarkerSize', 10);
+xlabel('popluation [10k]');
+ylabel('profit [$10k]');
 
 
 
